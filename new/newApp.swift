2340740e24f4ac981cd9cab8553b75a28d4580fa
andyjung <andyjung1129@naver.com>
2023-03-19 15:56:16 +0900

    Initial Commit

---
+++ new/newApp.swift
@@ -0,0 +1,17 @@
+//
+//  newApp.swift
+//  new
+//
+//  Created by 정재민 on 2023/03/19.
+//
+
+import SwiftUI
+
+@main
+struct newApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
